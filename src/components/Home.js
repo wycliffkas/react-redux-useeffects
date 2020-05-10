f58db0dd244b2d653c0a312d21b6c19c877f2c35
wycliffkas <w4wycliff@gmail.com>
2020-05-10 22:07:35 +0300

    using redux with class components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,36 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import * as action from "../store/actions/index";
 
-const Home = () => {
-  const currentState = useSelector((state) => state);
-  const dispatch = useDispatch();
+class Home extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div>
+          <h3>Count: {this.props.count}</h3>
+        </div>
+        <div>
+          <button onClick={this.props.inc}>+</button>
+          <button onClick={() => this.props.addNbr(5)}>Add 5</button>
+          <button onClick={this.props.dec}>-</button> 
+        </div>
+      </div>
+    );
+  }
+}
 
-  return (
-    <div className="container">
-      <div>
-        <h3>Count: {currentState.change.count}</h3>
-      </div>
-      <div>
-        <button onClick={() => dispatch(action.increment())}>+</button>
-        <button onClick={() => dispatch(action.increaseByNumber(5))}>
-          Add 5
-        </button>
-        <button onClick={() => dispatch(action.decrement())}>-</button>
-      </div>
-    </div>
-  );
+const mapStateToProps = (state) => {
+  return {
+    count: state.change.count,
+  };
 };
 
-export default Home;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    inc: () => dispatch(action.increment()),
+    dec: () => dispatch(action.decrement()),
+    addNbr: (x) => dispatch(action.increaseByNumber(x)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
